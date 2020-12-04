a79fb6b1410f5d7740c4fb4322b9ea90cae58b71
ccaulk <caca6275@colorado.edu>
2020-12-04 14:02:50 -0700

    small additions

--- code/Heap.h
+++ code/Heap.h
@@ -2,15 +2,22 @@
 #define Heap_H
 #include <iostream>
 #include <queue>
+#include <vector>
 #include "Resort.h"
 
 using namespace std;
+//compare function for the built in priority queue
+struct maxCurrent{
+    bool operator()(Resort r1, Resort r2) const{
+        return r1.getBaseDepth() > r2.getBaseDepth();
+    }
+};
 
 class Heap{
 
 public:
     
 private:
-    //priority_queue <Resort> predictedSnow;
+    priority_queue<Resort, vector<Resort>, maxCurrent> maxBase;
 };
 #endif
